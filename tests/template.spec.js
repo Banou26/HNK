7cda85dc90b6feac685ea2c9dd5170e18cbe8fdd
unknown <banoushirosaki@gmail.com>
2017-12-01 02:47:12 +0100

    add template array rendering

--- tests/template.spec.js
+++ tests/template.spec.js
@@ -5,173 +5,57 @@ chai.should()
 let assert = chai.assert
 
 describe('Template', function () {
-  describe('build function (literal tag)', function () {
-    it('should build text nodes', function () {
-      expect(_ => html`bar ${'foo'} bar2 ${'foo2'} bar3 ${'foo3'} bar4`).to.not.throw()
-    })
-
-    it('should build comment node', function () {
-      expect(_ => html`<!-- bar ${'foo'} bar2 ${'foo2'} bar3 ${'foo3'} bar4 -->`).to.not.throw()
-    })
-
-    it('should build element node', function () {
-      // html`<${'d'}i${'v'} a${'tt'}r="foo ${'bar'}"></${'d'}i${'v'}>`
-      expect(_ => html`<${'d'}i${'v'} a${'tt'}r="foo ${'bar'}"></${'d'}i${'v'}>`).to.not.throw()
-    })
-
-    it('should build instance', function () {
-      expect(_ => html`${html`${'foo'}`}`).to.not.throw()
-    })
-
-    it('should build html', function () {
-      expect(_ => html`
-      <!-- ${'comment'} -->
-      ${'text'}
-      <${'d'}i${'v'} at${'tr'}="foo ${'bar'}" pr${'op'}=${{}}>${
-        html`<d${'iv'}>${'foo'}</di${'v'}>`
-      }</${'d'}i${'v'}>`).to.not.throw()
-    })
+  let container
+  beforeEach(function () {
+    container = document.createElement('div')
+    document.body.appendChild(container)
   })
-  describe('instance function', function () {
-    let container
-
-    beforeEach(function () {
-      container = document.createElement('div')
-      document.body.appendChild(container)
-    })
-    afterEach(function () {
-      document.body.removeChild(container)
-    })
-
-    it('should create text nodes', function () {
-      let instance = html`bar ${'foo'} bar2 ${'foo2'} bar3 ${'foo3'} bar4`()
-      container.appendChild(instance.content)
-      container.innerHTML.should.equal('bar foo bar2 foo2 bar3 foo3 bar4')
-    })
-
-    it('should create comment node', function () {
-      let instance = html`<!-- bar ${'foo'} bar2 ${'foo2'} bar3 ${'foo3'} bar4 -->`()
-      container.appendChild(instance.content)
-      container.innerHTML.should.equal('<!-- bar foo bar2 foo2 bar3 foo3 bar4 -->')
-    })
-
-    it('should create element node', function () {
-      let propValue = {}
-      let instance = html`<${'d'}i${'v'} at${'tr'}="foo ${'bar'}" pr${'op'}=${propValue} ${'on'}-${'click'}=${_ => console.log('click')}></${'d'}i${'v'}>`()
-      container.appendChild(instance.content)
-      container.innerHTML.should.equal('<div attr="foo bar"></div>')
-      container.children[0].prop.should.equal(propValue)
-    })
-
-    it('should create instance', function () {
-      let instance = html`${html`<d${'iv'}>${'foo'}</di${'v'}>`}`()
-      // let instance = html`${html`<div>${'foo'}</div>`}`()
-      // let instance = html`<div>${'foo'}</div>`()
-      // console.log(instance.childNodes, container)
-      container.appendChild(instance.content)
-      container.innerHTML.should.equal('<div>foo</div>')
-    })
-
-    it('should create html', function () {
-      let propValue = {}
-      let instance
-      let value = 0
-      expect(_ => (instance = html`
-      <!-- ${'comment'} -->
-      ${'text'}
-      <${'d'}i${'v'} at${'tr'}="foo ${'bar'}" pr${'op'}=${propValue} ${'on'}-${'click'}=${_ => (value = 5)}>
-        ${html`<d${'iv'}>${'foo'}</di${'v'}>`}
-      </${'d'}i${'v'}>`())).not.to.throw()
-      container.appendChild(instance.content)
-      container.innerHTML.should.equal(`
-      <!-- comment -->
-      text
-      <div attr="foo bar">
-        <div>foo</div>
-      </div>`)
-      container.children[0].click()
-      value.should.equal(5)
-      container.children[0].prop.should.equal(propValue)
-    })
+  afterEach(function () {
+    document.body.removeChild(container)
   })
 
-  describe('update function', function () {
-    let container
+  it('should render text nodes', function () {
+    let instance = html`${'foo'} bar ${'foo2'} bar2 ${'foo3'} bar3`()
+    container.appendChild(instance.content)
+    instance.update('foobar', 'foobar2', 'foobar3')
+    container.innerHTML.should.equal('foobar bar foobar2 bar2 foobar3 bar3')
+  })
 
-    beforeEach(function () {
-      container = document.createElement('div')
-      document.body.appendChild(container)
-    })
-    afterEach(function () {
-      document.body.removeChild(container)
-    })
+  it('should render comment node', function () {
+    let instance = html`<!-- ${'foo'} bar ${'foo2'} bar2 ${'foo3'} bar3 -->`()
+    container.appendChild(instance.content)
+    instance.update('foobar', 'foobar2', 'foobar3')
+    container.innerHTML.should.equal('<!-- foobar bar foobar2 bar2 foobar3 bar3 -->')
+  })
 
-    it('should update text nodes', function () {
-      let instance = html`${'foo'} bar ${'foo2'} bar2 ${'foo3'} bar3`()
-      container.appendChild(instance.content)
-      instance.update('foobar', 'foobar2', 'foobar3')
-      container.innerHTML.should.equal('foobar bar foobar2 bar2 foobar3 bar3')
-    })
+  it('should render element node', function () {
+    let propValue = {}
+    let instance = html`<${'div'} at${'tr'}="foo ${'bar'}" pr${'op'}=${propValue}>${'text'}<div></div></${'div'}>`()
+    container.appendChild(instance.content)
+    let newPropValue = {}
+    instance.update('span', 'tr2', 'foobar', 'op2', newPropValue, 'text2', 'span')
+    container.innerHTML.should.equal('<span attr2="foo foobar">text2<div></div></span>')
+    container.children[0].prop2.should.equal(newPropValue)
+  })
 
-    it('should update comment node', function () {
-      let instance = html`<!-- ${'foo'} bar ${'foo2'} bar2 ${'foo3'} bar3 -->`()
-      container.appendChild(instance.content)
-      instance.update('foobar', 'foobar2', 'foobar3')
-      container.innerHTML.should.equal('<!-- foobar bar foobar2 bar2 foobar3 bar3 -->')
-    })
+  it('should render instance', function () {
+    let instance = html`${html`<div>${'foo'}</div>`}`()
+    container.appendChild(instance.content)
+    instance.update(html`<span>${'bar'}</span>`)
+    container.innerHTML.should.equal('<span>bar</span>')
+  })
 
-    it('should update element node', function () {
-      let propValue = {}
-      let instance = html`<${'div'} at${'tr'}="foo ${'bar'}" pr${'op'}=${propValue}>${'text'}<div></div></${'div'}>`()
-      container.appendChild(instance.content)
-      let newPropValue = {}
-      instance.update('span', 'tr2', 'foobar', 'op2', newPropValue, 'text2', 'span')
-      container.innerHTML.should.equal('<span attr2="foo foobar">text2<div></div></span>')
-      container.children[0].prop2.should.equal(newPropValue)
-    })
+  it('should render dom nodes', function () {
+    let instance = html`<div>${new Text('foo')}</div>`()
+    container.appendChild(instance.content)
+    instance.update(new Text('bar'))
+    container.innerHTML.should.equal('<div>bar</div>')
+  })
 
-    it('should update instance', function () {
-      let instance = html`${html`<d${'iv'}>${'foo'}</di${'v'}>`}`()
-      container.appendChild(instance.content)
-      instance.update(html`<span>${'bar'}</sp${'an'}>`)
-      container.innerHTML.should.equal('<span>bar</span>')
-    })
-
-    it('should update html', function () {
-      let propValue = {}
-      let value = 0
-      let childDiv = html`<d${'iv'}>${'foo'}</di${'v'}>`
-      // console.log('====')
-      let instance = html`
-      <!-- ${'comment'} -->
-      ${'text'}
-      <${'div'} at${'tr'}="foo ${'bar'}" pr${'op'}=${propValue} ${'on'}-${'click'}=${_ => (value = 5)}>${'text'}${childDiv}</${'div'}>`()
-      container.appendChild(instance.content)
-      let newPropValue = {}
-      container.children[0].click()
-      value.should.equal(5)
-      container.innerHTML.replace(/\s+/g, '').should.equal(`
-      <!-- comment -->
-      text
-      <div attr="foo bar">
-        text<div>foo</div>
-        
-      </div>`.replace(/\s+/g, ''))
-      container.children[0].prop.should.equal(propValue)
-      instance.update('comment2', 'text2', 'span', 'tr2', 'foobar', 'op2', newPropValue, 'on', 'focus', _ => (value = 10), 'text2', childDiv, 'span')
-      container.innerHTML.replace(/\s+/g, '').should.equal(`
-      <!-- comment2 -->
-      text2
-      <span attr2="foo foobar">
-        text2<div>foo</div>
-        
-      </span>`.replace(/\s+/g, ''))
-      value = 0
-      container.children[0].contentEditable = 'true'
-      container.children[0].focus()
-      container.children[0].click()
-      value.should.equal(10)
-      container.children[0].prop2.should.equal(newPropValue)
-    })
+  it('should render arrays items', function () {
+    let instance = html`${['foo', new Comment('bar'), document.createElement('div')]}`()
+    container.appendChild(instance.content)
+    instance.update(['foo2', new Comment('bar2'), document.createElement('span')])
+    container.innerHTML.should.equal(`foo2<!--bar2--><span></span>`)
   })
 })
