ffdf5d5882959c03cf5ae14b500f88e40e6c5211
Banou26 <me@banou.dev>
2017-12-23 07:34:58 +0100

    Fix object type testing

--- src/util/index.js
+++ src/util/index.js
@@ -11,7 +11,7 @@ export function cloneObject (original) {
     let value, rest
     if (desc) ({value, ...rest} = desc)
     else value = original[i]
-    if (typeof value === 'object') value = cloneObject(value)
+    if (!original || typeof value === 'object') value = cloneObject(value)
     if (desc) Object.defineProperty(copiedProps, i, {...rest, ...value && {value}})
     else copiedProps[i] = value
   }
