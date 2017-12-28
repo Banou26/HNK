be2faea1246f36e1a1f83764a4612f9c1e72a5de
unknown <banoushirosaki@gmail.com>
2017-12-28 18:25:18 +0100

    Add router

--- src/elements/router-view.js
+++ src/elements/router-view.js
@@ -1,11 +1,54 @@
 import { Element } from './element.js'
+import { html } from '../template/html'
+
+const nthRouterView = routerView => {
+  let nth = 0
+  let elem = routerView.parentElement
+  while (elem) {
+    if (elem instanceof RouterView && elem.router === routerView.router) nth++
+    elem = elem.parentElement
+  }
+  return nth
+}
+
+const getRouteConfigNthParent = (routeConfig, nth) => {
+  let elem = routeConfig
+  while (nth) {
+    elem = elem.parent
+    nth--
+  }
+  return elem
+}
 
 export class RouterView extends Element {
-  constructor () {
-    super()
+  constructor (router) {
+    super({router})
   }
 
-  static template () {
-    return this.router
+  set router (router) {
+    this.state.router = router
+    super.router = router
+  }
+
+  get components () {
+    return this.state.components
+  }
+
+  state () {
+    const _this = this
+    return {
+      router: null,
+      get components () {
+        return this.router && this.router.currentRoute.matched && getRouteConfigNthParent(this.router.currentRoute.matched.components, nthRouterView(_this))
+      }
+    }
+  }
+
+  static template (state) {
+    const elems = []
+    if (state.router && state.components) {
+      for (const Component of state.components) elems.push(new Component({router: state.router}))
+    }
+    return html`${elems.length ? elems : ''}`
   }
 }
