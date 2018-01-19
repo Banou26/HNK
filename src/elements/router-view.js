021fe265678dd92ca78618917ddc40cdb549005f
unknown <banoushirosaki@gmail.com>
2018-01-19 04:48:55 +0100

    Clean router-view

--- src/elements/router-view.js
+++ src/elements/router-view.js
@@ -1,6 +1,5 @@
-import { Element, registerElement } from './element.js'
-import { html } from '../template/html'
-import { defaultReactiveRoot } from '../reactivity/index.js'
+import { registerElement } from './element.js'
+import { html } from '../template/html.js'
 
 const nthRouterView = routerView => {
   let nth = 0
@@ -21,43 +20,6 @@ const getRouteConfigNthParent = (routeConfig, nth) => {
   return elem
 }
 
-// export class RouterView extends Element {
-//   constructor (router) {
-//     super({router})
-//   }
-
-//   set $router (router) {
-//     this.$state.router = router
-//     super.$router = router
-//   }
-
-//   get $router () {
-//     return this.$state.router
-//   }
-
-//   get components () {
-//     return this.$state.components
-//   }
-
-//   state () {
-//     const _this = this
-//     return {
-//       router: null,
-//       get components () {
-//         return this.router && this.router.currentRoute.matched && getRouteConfigNthParent(this.router.currentRoute.matched.components, nthRouterView(_this))
-//       }
-//     }
-//   }
-
-//   static template ({state: {router, components}}) {
-//     const elems = []
-//     if (router && components) {
-//       for (const Component of components) elems.push(Component instanceof HTMLElement ? new Component({router: router}) : new (customElements.get(Component.name))())
-//     }
-//     return html`${elems.length ? elems : ''}`
-//   }
-// }
-
 const template = ({state: {components}}) => {
   const elems = []
   if (components) {
@@ -69,7 +31,7 @@ const template = ({state: {components}}) => {
 export const RouterView = customElements.get('router-view') || registerElement({
   name: 'router-view',
   template,
-  state: (ctx) => ({
+  state: ctx => ({
     get components () {
       const {router, host} = ctx
       return router && router.currentRoute.matched && getRouteConfigNthParent(router.currentRoute.matched.components, nthRouterView(host))
