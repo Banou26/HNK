d0d52aea50c25d3e60105174528e9ea1e03b9ac5
unknown <banoushirosaki@gmail.com>
2018-02-05 20:13:57 +0100

    Comment router-link until rework

--- src/elements/router-link.js
+++ src/elements/router-link.js
@@ -1,41 +1,41 @@
-import { Element } from './element.js'
-import { html } from '../template/html.js'
-import { css } from '../template/css.js'
+// import { Element } from './element.js'
+// import { html } from '../template/html.js'
+// import { css } from '../template/css.js'
 
-export class RouterLink extends Element {
-  constructor (href, router) {
-    super({ shadowDom: 'open' })
-    this.router = router
-    this.href = href
-    this.addEventListener('click', ev => {
-      if (!this.router) throw new Error('No router defined for this router-link')
-      this.router.push(this.href)
-    })
-  }
+// export class RouterLink extends Element {
+//   constructor (href, router) {
+//     super({ shadowDom: 'open' })
+//     this.router = router
+//     this.href = href
+//     this.addEventListener('click', ev => {
+//       if (!this.router) throw new Error('No router defined for this router-link')
+//       this.router.push(this.href)
+//     })
+//   }
 
-  static get observedAttributes () { return ['href'] }
+//   static get observedAttributes () { return ['href'] }
 
-  attributeChangedCallback (attr, oldValue, newValue) {
-    if (attr === 'href') this.href = newValue
-  }
+//   attributeChangedCallback (attr, oldValue, newValue) {
+//     if (attr === 'href') this.href = newValue
+//   }
 
-  static template () {
-    return html`<slot></slot>`
-  }
+//   static template () {
+//     return html`<slot></slot>`
+//   }
 
-  static style () {
-    return css`
-    :host {
-      cursor: pointer;
-    }
-    `
-  }
+//   static style () {
+//     return css`
+//     :host {
+//       cursor: pointer;
+//     }
+//     `
+//   }
 
-  set href (href) {
-    this._href = href
-  }
+//   set href (href) {
+//     this._href = href
+//   }
 
-  get href () {
-    return this._href
-  }
-}
+//   get href () {
+//     return this._href
+//   }
+// }
