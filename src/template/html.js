bddd6ef92b3bccdeb39c3cb8be640b8592d2ee9a
unknown <banoushirosaki@gmail.com>
2018-03-08 17:25:39 +0100

    Refactor html template

--- src/template/html.js
+++ src/template/html.js
@@ -62,48 +62,110 @@ const getPlaceholderWithPaths = (node, _placeholders) => {
   }
   return [...paths].map(([placeholder, path]) => ({...placeholder, path}))
 }
-// =======================================RECODE THIS PART=======================================
-const patchDomArray = (newArray, oldArray) => {
-  const nodesToRemove = oldArray.filter(node => !newArray.includes(node))
-  for (const i in newArray) {
-    const newNode = newArray[i]
-    const oldNode = oldArray[i]
-    if (newNode !== oldNode) {
-      if (oldNode && oldNode.parentNode) {
-        oldNode.parentNode.insertBefore(newNode, oldNode)
-        oldNode.parentNode.removeChild(oldNode)
-      } else {
-        const previousNewNode = newArray[i - 1]
-        if (previousNewNode && previousNewNode.parentNode) {
-          previousNewNode.parentNode.insertBefore(newNode, previousNewNode.nextSibling)
-          if (oldNode && oldNode.parentNode) oldNode.parentNode.removeChild(oldNode)
-        }
-      }
-    }
-  }
-  for (const node of nodesToRemove) {
-    if (node && node.parentNode) node.parentNode.removeChild(node)
-  }
-}
-// =======================================/RECODE THIS PART/=======================================
-const newPlaceholdersNodes = (oldMap, placeholder, nodes) => {
-  const newMap = new Map(oldMap)
-  newMap.set(placeholder, nodes)
-  return newMap
-}
 
 const createInstance = ({ id, template, placeholders }, ...values) => {
   const doc = document.importNode(template.content, true)
   let bypassDif = true
   let childNodes = [...doc.childNodes]
   let listeners = []
-  let placeholdersNodes = new Map(placeholders.map(placeholder => ([placeholder,
-    placeholder.type === 'text'
-    ? [getNode(doc, placeholder.path)]
-    : getNode(doc, placeholder.path)
-  ])))
+  let placeholdersNodes = new Map(placeholders.map(placeholder => (
+    [
+      placeholder,
+      placeholder.type === 'text' ? [getNode(doc, placeholder.path)] : getNode(doc, placeholder.path)
+    ]
+  )))
   let placeholdersData = new Map(placeholders.map(placeholder => [placeholder, {}]))
   let placeholderByIndex = indexPlaceholders(placeholders)
+
+  const updatePlaceholder = ({ values, placeholder }) => {
+    const currentData = placeholdersData.get(placeholder)
+    if (currentData && currentData.directive) currentData.directive() // cleanup directive function
+    const index = placeholder.index || placeholder.indexes[0]
+    const directive = values[index]
+    const data = placeholdersData.get(placeholder)
+    const node = placeholdersNodes.get(placeholder)
+
+    const replaceNode = (newNode, node) => {
+      placeholdersNodes = new Map([...placeholdersNodes, [placeholder, newNode]]
+        .map(([_placeholder, _node]) => node === _node
+        ? [_placeholder, newNode]
+        : [_placeholder, _node])
+      )
+      childNodes = Object.assign([...childNodes], {[childNodes.indexOf(node)]: newNode})
+      const { parentNode } = node
+      if (parentNode) {
+        parentNode.insertBefore(newNode, node)
+        parentNode.removeChild(node)
+      }
+    }
+
+    const replaceNodes = (newArray, oldArray) => {
+      placeholdersNodes = new Map([...placeholdersNodes, [placeholder, newArray]])
+      childNodes = Object.assign([...childNodes], {[childNodes.indexOf(oldArray)]: newArray})
+      newArray = flattenArray(newArray)
+      oldArray = flattenArray(oldArray)
+      const nodesToRemove = oldArray.filter(node => !newArray.includes(node))
+      for (const i in newArray) {
+        const newNode = newArray[i]
+        const oldNode = oldArray[i]
+        if (newNode !== oldNode) {
+          if (oldNode && oldNode.parentNode) {
+            oldNode.parentNode.insertBefore(newNode, oldNode)
+            oldNode.parentNode.removeChild(oldNode)
+          } else {
+            const previousNewNode = newArray[i - 1]
+            if (previousNewNode && previousNewNode.parentNode) {
+              previousNewNode.parentNode.insertBefore(newNode, previousNewNode.nextSibling)
+              if (oldNode && oldNode.parentNode) oldNode.parentNode.removeChild(oldNode)
+            }
+          }
+        }
+      }
+      for (const node of nodesToRemove) {
+        if (node && node.parentNode) node.parentNode.removeChild(node)
+      }
+    }
+
+    const setElement = newElement => {
+      const element = placeholdersNodes.get(placeholder)
+      const elementPlaceholders = placeholder.attributes.map(indexes => placeholderByIndex[indexes[0]])
+      for (const {name, value} of element.attributes) newElement.setAttribute(name, value)
+      for (const childNode of element.childNodes) newElement.appendChild(childNode)
+      replaceNode(newElement, element)
+      for (const placeholder of elementPlaceholders) updatePlaceholder({values, placeholder})
+    }
+
+    if (placeholder.type === 'attribute' && placeholder.indexes.length === 1 && directive && directive.directive) { // placeholder value is a directive
+      placeholdersData = new Map([...placeholdersData, [
+        placeholder,
+        { directive: directive({ getElement: placeholdersNodes.get.bind(placeholdersNodes, placeholder), setElement }) }
+      ]])
+    } else {
+      const updateResult = update[placeholder.type]({
+        placeholder,
+        values,
+        data,
+        [placeholder.type === 'text' ? 'nodes' : 'node']: node,
+        placeholderByIndex,
+        getChildNodes () { return instance._childNodes },
+        setChildNodes: newChildNodes => {
+          const _childNodes = childNodes
+          childNodes = newChildNodes
+          for (const listener of listeners) listener(childNodes, _childNodes)
+        }
+      })
+      if (placeholder.type === 'text') {
+        if (node !== updateResult.nodes) replaceNodes(updateResult.nodes, node)
+      } else if (placeholder.type === 'tag' || placeholder.type === 'attribute') {
+        if (node !== updateResult.node) setElement(updateResult.node)
+      } else {
+        if (node !== updateResult.node) replaceNode(updateResult.node, node)
+      }
+      placeholdersData = new Map([...placeholdersData, [placeholder, updateResult.data]])
+    }
+    for (const listener of listeners) listener(childNodes, node)
+  }
+
   const instance = {
     id,
     values,
@@ -122,79 +184,8 @@ const createInstance = ({ id, template, placeholders }, ...values) => {
       : getValueIndexDifferences(values, instance.values) // placeholders which split values has changed
         .map(index => placeholderByIndex[index]) // placeholders
         .filter(placeholder => placeholder && placeholdersNodes.get(placeholder))
-        .reduce((arr, placeholder) => [...arr,
-          ...placeholder.type === 'tag'
-          ? [placeholder, ...placeholder.attributes.map(indexes => placeholderByIndex[indexes[0]])]
-          : [placeholder]
-        ], []) // placeholders with attributes of tags that need update
-        .reduce((arr, placeholder) => [...arr, ...arr.includes(placeholder) ? [] : [placeholder]], []) // remove duplicates
       instance.values = values
-      const placeholderByOldNode = new Map(placeholdersToUpdate.map(placeholder => [placeholdersNodes.get(placeholder), placeholder]))
-      const oldNodeByPlaceholder = new Map([...placeholderByOldNode].map(([node, placeholder]) => [placeholder, node]))
-      const updateResults = new Map(placeholdersToUpdate.map(placeholder => {
-        const result = update[placeholder.type]({
-          placeholder,
-          values,
-          data: placeholdersData.get(placeholder),
-          [placeholder.type === 'text' ? 'nodes' : 'node']: placeholdersNodes.get(placeholder),
-          placeholderByIndex,
-          getChildNodes () { return instance._childNodes },
-          setChildNodes: _childNodes => {
-            // patchDomArray(flattenArray(_childNodes), flattenArray(childNodes))
-            const oldChildNodes = childNodes
-            childNodes = _childNodes
-            for (const listener of listeners) listener(_childNodes, oldChildNodes)
-          }
-        })
-        if ((result.node || result.nodes) === placeholdersNodes.get(placeholder)) return [placeholder, result]
-        if (placeholder.type === 'tag' || placeholder.type === 'attribute') {
-          if (placeholder.tag && placeholder.tag.length) {
-            placeholdersNodes = newPlaceholdersNodes(placeholdersNodes, placeholderByIndex[placeholder.tag[0]], result.node)
-          }
-          for (const attrIndex of placeholder.attributes) {
-            placeholdersNodes = newPlaceholdersNodes(placeholdersNodes, placeholderByIndex[attrIndex[0]], result.node)
-            // const oldNode = oldNodeByPlaceholder.get(placeholderByIndex[attrIndex[0]])
-            // if (childNodes.includes(oldNode)) {
-            //   childNodes = [...childNodes]
-            //   childNodes.splice(childNodes.indexOf(oldNode), 1, result.node)
-            // }
-          }
-        }
-        placeholdersNodes = newPlaceholdersNodes(placeholdersNodes, placeholder, placeholder.type === 'text' ? result.nodes : result.node)
-        // const oldNode = oldNodeByPlaceholder.get(placeholder)
-        // if (childNodes.includes(oldNode)) {
-        //   childNodes = [...childNodes]
-        //   childNodes.splice(childNodes.indexOf(oldNode), 1, result.node)
-        // }
-        return [placeholder, result]
-      }))
-      placeholdersData = new Map([...placeholdersData].map(([placeholder, data]) => [placeholder,
-        updateResults.has(placeholder)
-        ? updateResults.get(placeholder).data
-        : data
-      ]))
-      // =======================================RECODE THIS PART=======================================
-      for (const [placeholder, updateResult] of [...updateResults].filter(([placeholder]) => placeholder.type === 'text')) {
-        patchDomArray(flattenArray(updateResult.nodes), flattenArray(oldNodeByPlaceholder.get(placeholder)))
-      }
-      for (const [placeholder, updateResult] of [...updateResults].filter(([placeholder]) => placeholder.type !== 'text')) {
-        const oldNode = oldNodeByPlaceholder.get(placeholder)
-        if (oldNode && oldNode.parentNode) oldNode.parentNode.insertBefore(updateResult.node, oldNode)
-      }
-      const newChildNodes = [...childNodes].map(item => {
-        if (placeholderByOldNode.has(item)) {
-          const placeholder = placeholderByOldNode.get(item)
-          const result = updateResults.get(placeholder)
-          return result.node || result.nodes
-        } else {
-          return item
-        }
-      })
-      patchDomArray(flattenArray(newChildNodes), flattenArray(childNodes))
-      // =======================================/RECODE THIS PART/=======================================
-      const oldChildNodes = childNodes
-      childNodes = newChildNodes
-      for (const listener of listeners) listener(newChildNodes, oldChildNodes)
+      for (const placeholder of placeholdersToUpdate) updatePlaceholder({placeholder, values})
     },
     listen (func) {
       listeners = [...listeners, func]
@@ -246,7 +237,7 @@ export const html = htmlTemplate(str => str)
 const update = {
   comment ({ values, node, placeholder: { split } }) {
     node.nodeValue = mergeSplitWithValues(split, values)
-    return {node}
+    return { node }
   },
   text ({
     value,
@@ -275,17 +266,15 @@ const update = {
       } else {
         const instance = value()
         const unlisten = instance.listen((newChildNodes, oldChildNodes) => {
-          const newNodes = getChildNodes().slice()
-          newNodes[newNodes.indexOf(oldChildNodes)] = newChildNodes
-          setChildNodes(newNodes)
+          setChildNodes(newChildNodes)
+          // const currentChildNodes = getChildNodes()
+          // setChildNodes(Object.assign([...currentChildNodes], {[currentChildNodes.indexOf(oldChildNodes)]: newChildNodes}))
         })
         return { nodes: instance._childNodes, data: { instance, unlisten } }
       }
     } else if (value && value.instance) {
       const unlisten = value.listen((newChildNodes, oldChildNodes) => {
-        const newNodes = getChildNodes().slice()
-        newNodes[newNodes.indexOf(oldChildNodes)] = newChildNodes
-        setChildNodes(newNodes)
+        setChildNodes(newChildNodes)
       })
       return { nodes: value._childNodes, data: { instance: value, unlisten } }
     } else if (Array.isArray(value)) {
@@ -304,11 +293,13 @@ const update = {
       return { nodes: [ nodes[0] instanceof Comment ? nodes[0] : new Comment('') ] }
     }
   },
-  tag ({ values, node: _node, placeholderByIndex, placeholder: { attributes, split } }) {
-    const node = document.createElement(mergeSplitWithValues(split, values))
-    for (const {name, value} of _node.attributes) node.setAttribute(name, value)
-    for (const childNode of _node.childNodes) node.appendChild(childNode)
-    return {node}
+  tag: ({ values, node, placeholder: { split } }) => {
+    const newTag = mergeSplitWithValues(split, values)
+    return {
+      node: node.tagName.toLowerCase() === newTag.toLowerCase()
+        ? node
+        : document.createElement(newTag)
+    }
   },
   attribute ({ values, placeholder, node, data: { name: oldName, listener: oldListener, value: oldValue } = {}, placeholder: { attributeType, nameSplit, valueSplit } }) {
     // todo: add event listeners feature
