5230977763368600ad97219022851595d3f35002
Banou26 <me@banou.dev>
2018-03-30 06:05:50 +0200

    Rework router part 1 (messy code)

--- src/elements/router-view.js
+++ src/elements/router-view.js
@@ -1,28 +1,23 @@
 import { registerElement } from './element.js'
 import { html } from '../template/html.js'
 
-const getRouterViewPosition = ({parentElement}, n = 0) => parentElement ? getRouterViewPosition(parentElement, n + 1) : n
-const getNthElement = (routeConfig, n) => n ? getNthElement(routeConfig.parent, n - 1) : routeConfig
+const getRouterViewPosition = ({parentElement}, n = 0) => parentElement
+  ? getRouterViewPosition(parentElement, n + parentElement instanceof RouterView ? 1 : 0)
+  : n
+const getRouterViewPositionElement = (route, n) => n ? getRouterViewPositionElement(route.parent, n - 1) : route
 
-const template = ({state: {childs}}) => html`${childs}`
-
-const createComponents = components => components && components.map(Component => document.createElement(Component.name))
+const template = ({state: {components}}) => html`${components}`
 
 export const RouterView = customElements.get('router-view') || registerElement({
   name: 'router-view',
+  props: ['name'],
   template,
   state: ctx => ({
-    childs: undefined,
     get components () {
-      const { matched } = ctx.router
-      return matched && getNthElement(matched.components, getRouterViewPosition(ctx.host))
+      const { router: { currentRoutesComponents, currentRoute: { matched } = {} } = {}, props: { name = 'default' } } = ctx
+      if (matched) {
+        return currentRoutesComponents.get(matched[getRouterViewPosition(ctx.host)]/* route */)/* components */.get(name)/* component */
+      }
     }
-  }),
-  watchers: [
-    [
-      ({state: {components}}) => (components),
-      ({state}) => (state.childs = createComponents(state.components))
-    ]
-  ],
-  created: ({state}) => (state.childs = createComponents(state.components))
+  })
 })
