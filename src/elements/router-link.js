af9cc7f98b9bb3654796fdf586f442872fca6c24
Banou <banoushirosaki@gmail.com>
2018-05-08 02:33:22 +0200

    Refactor router-link

--- src/elements/router-link.js
+++ src/elements/router-link.js
@@ -1,41 +1,7 @@
-// import { Element } from './element.js'
-// import { html } from '../template/html.js'
-// import { css } from '../template/css.js'
+import { registerElement } from './element.js'
+import { css } from '../template/css.js'
 
-// export class RouterLink extends Element {
-//   constructor (href, router) {
-//     super({ shadowDom: 'open' })
-//     this.router = router
-//     this.href = href
-//     this.addEventListener('click', ev => {
-//       if (!this.router) throw new Error('No router defined for this router-link')
-//       this.router.push(this.href)
-//     })
-//   }
-
-//   static get observedAttributes () { return ['href'] }
-
-//   attributeChangedCallback (attr, oldValue, newValue) {
-//     if (attr === 'href') this.href = newValue
-//   }
-
-//   static template () {
-//     return html`<slot></slot>`
-//   }
-
-//   static style () {
-//     return css`
-//     :host {
-//       cursor: pointer;
-//     }
-//     `
-//   }
-
-//   set href (href) {
-//     this._href = href
-//   }
-
-//   get href () {
-//     return this._href
-//   }
-// }
+export default registerElement({
+  name: 'router-link',
+  style: _ => css`router-link { cursor: pointer; }`
+})
