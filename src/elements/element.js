fa0bad94708a35282ad9646e9c0ae349312e48d3
Banou26 <banoushirosaki@gmail.com>
2018-07-13 11:45:14 +0200

    Implement OzElement

--- src/elements/element.js
+++ src/elements/element.js
@@ -1,8 +1,6 @@
 import registerClass from './class.js'
 import registerObject from './object.js'
 
-export const elementContext = Symbol.for('OzElementContext')
-
 export default element =>
   typeof element === 'function'
     ? registerClass(element)
