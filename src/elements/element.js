274391213728a728380cc2fcf9809669522b3d2c
Banou26 <me@banou.dev>
2018-07-25 16:04:26 +0200

    Remove the OzElement class implementation and improve the default one

--- src/elements/element.js
+++
@@ -1,7 +0,0 @@
-import registerClass from './class.js'
-import registerObject from './object.js'
-
-export default element =>
-  typeof element === 'function'
-    ? registerClass(element)
-    : registerObject(element)
