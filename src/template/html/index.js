260e7df19ede2ec7dee29f3ec26ab039d2283f44
Banou26 <me@banou.dev>
2018-08-03 15:13:56 +0200

    Go back to minimalist implementation of the html parser part 1

--- src/template/html/index.js
+++ src/template/html/index.js
@@ -1,5 +1,5 @@
 import { placeholder } from '../utils.js'
-import analyze from './analyzer.js'
+import parse from './parser.js'
 import createTemplate, { OzHTMLTemplate } from './elements/template.js'
 
 export {
@@ -9,9 +9,9 @@ export {
 const templates = new Map()
 
 export const tag = (transform = str => str) => (strings, ...values) => {
-  const templateId = strings.join(placeholder())
+  const templateId = strings.reduce((str, str2, i) => str + placeholder(i - 1) + str2)
   if (templates.has(templateId)) return templates.get(templateId).clone(values)
-  const { html, placeholders } = analyze({ transform, strings, values })
+  const { html, placeholders } = parse({ transform, strings, values })
   templates.set(templateId, createTemplate({templateId, html, values, placeholders}))
   return templates.get(templateId).clone(values)
 }
