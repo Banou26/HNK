de346fdcf99597ac36bc807410a28d15fdbc9868
Banou26 <banoushirosaki@gmail.com>
2018-08-22 06:13:28 +0200

    Use chai-dom for the html template testing

--- wallaby.js
+++ wallaby.js
@@ -3,6 +3,7 @@ module.exports = function (wallaby) {
     debug: true,
     files: [
       {pattern: 'node_modules/chai/chai.js', instrument: false},
+      {pattern: 'node_modules/chai-dom/chai-dom.js', instrument: false},
       './src/**/[^_]*.js'
     ],
     tests: [
