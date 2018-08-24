f9355c6df4fcb95755ef3697c153de9bbc887bec
Banou26 <me@banou.dev>
2018-08-24 06:04:03 +0200

    Small lint fix

--- src/template/html/index.js
+++ src/template/html/index.js
@@ -13,7 +13,7 @@ export const tag = (transform = str => str) => (strings, ...values) => {
   const templateId = 'html' + strings.reduce((str, str2, i) => str + placeholder(i - 1) + str2)
   if (templates.has(templateId)) return templates.get(templateId).clone(values)
   const { fragment, placeholdersMetadata } = parse({ transform, strings, values })
-  templates.set(templateId, createTemplate({templateId, originalFragment: fragment, values, placeholdersMetadata}))
+  templates.set(templateId, createTemplate({ templateId, originalFragment: fragment, values, placeholdersMetadata }))
   return templates.get(templateId).clone(values)
 }
 
