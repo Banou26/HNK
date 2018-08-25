98b88c17480fdc095d23695e95d412d8e6a439d3
Banou26 <banoushirosaki@gmail.com>
2018-08-25 05:32:30 +0200

    Change outdated reactify test

--- tests/reactivity.spec.js
+++ tests/reactivity.spec.js
@@ -1,8 +1,8 @@
 import { r, reactivity } from '../src/index.js'
 
 describe('reactify', () => {
-  it('accept an option = { immutable: Boolean } argument', () => {
-    expect(_ => r({}, { immutable: true })).to.not.throw()
+  it('accept no parameters at all', () => {
+    expect(_ => r()).to.not.throw()
   })
 })
 
