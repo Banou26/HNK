240540950486aae44fd0dc4ab749fbc2ef03eb5c
Banou26 <thomas26_ds@msn.com>
2018-09-10 18:47:19 +0200

    Clean the repo

--- src/template/html/elements/empty-fragment.js
+++
@@ -1,8 +0,0 @@
-export const OzEmptyFragmentSymbol = Symbol.for('OzEmptyFragment')
-
-export class OzEmptyFragment extends Comment {
-  constructor () { super('OzEmptyFragment') }
-  get [OzEmptyFragmentSymbol] () { return true }
-}
-
-export default OzEmptyFragment
