cb2aa2e126e470f3fbcc43f9639284e06a02c837
Banou26 <me@banou.dev>
2018-09-26 20:47:24 +0200

    Fix variable naming

--- src/template/css/index.js
+++ src/template/css/index.js
@@ -6,14 +6,14 @@ export {
   OzStyle
 }
 
-const elements = new Map()
+const styles = new Map()
 
 export const CSSTag = (transform = str => str) => (strings, ...values) => {
   const templateId = 'css' + strings.reduce((str, str2, i) => str + placeholder(i - 1) + str2)
-  if (elements.has(templateId)) return elements.get(templateId).clone(values)
+  if (styles.has(templateId)) return styles.get(templateId).clone(values)
   const { ast, css, placeholdersMetadata } = parse({ transform, strings, values })
-  elements.set(templateId, createStyle({ templateId, css, values, ast, placeholdersMetadata }))
-  return elements.get(templateId).clone(values)
+  styles.set(templateId, createStyle({ templateId, css, values, ast, placeholdersMetadata }))
+  return styles.get(templateId).clone(values)
 }
 
 export const css = CSSTag()
