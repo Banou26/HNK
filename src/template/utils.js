c2b2988f3768009987b1ee2572979a2f64d3186d
Banou26 <me@banou.dev>
2018-10-23 05:34:38 +0200

    Add second range of placeholders

--- src/template/utils.js
+++ src/template/utils.js
@@ -1,3 +1,4 @@
+// placeholders first range U+E000..U+F8FF
 export const placeholderMinRangeChar = ''
 export const placeholderMinRangeCode = placeholderMinRangeChar.charCodeAt()
 
@@ -20,3 +21,30 @@ export const toPlaceholderString =
 
 export const replace = (arrayFragment, ...vals) =>
   arrayFragment.splice(0, arrayFragment.length, ...vals)
+
+// placeholders first range U+E000..U+F8FF //
+
+// placeholders second range U+F0000..U+FFFFD
+export const placeholder2MinRangeChar = '󰀀'
+export const placeholder2MinRangeCode = placeholder2MinRangeChar.charCodeAt()
+
+export const placeholder2MaxRangeChar = '󿿽'
+export const placeholder2MaxRangeCode = placeholder2MaxRangeChar.charCodeAt()
+
+export const placeholder2Regex = new RegExp(`[${placeholder2MinRangeChar}-${placeholder2MaxRangeChar}]`, 'umg') // /[󰀀-󿿽]/umg
+export const singlePlaceholder2Regex = new RegExp(placeholder2Regex, 'um')
+
+export const placeholder2 = (n = 0) => String.fromCodePoint(placeholder2MinRangeCode + n)
+
+export const charToN2 = str => str.codePointAt() - placeholder2MinRangeCode
+
+export const matchSelectorRulesets = (str, matchedStrRuleset = []) =>
+  str
+    .replace(/(".*?"|'.*?')/g, (_, str) =>
+      placeholder2(matchedStrRuleset.push(str)))
+    .split(',')
+    .map(str =>
+      str.replace(placeholder2Regex, char =>
+        matchedStrRuleset[charToN2(char) - 1])
+        .trim())
+// placeholders second range U+F0000..U+FFFFD //
