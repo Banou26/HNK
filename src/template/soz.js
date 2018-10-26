5a2cfd73c9fd9dfd4396d035a39a34316ad6d01e
Banou26 <thomas26_ds@msn.com>
2018-10-26 05:52:15 +0200

    Only set style selectors with any/matches if there's multiple selectors

--- src/template/soz.js
+++ src/template/soz.js
@@ -14,17 +14,22 @@ const makeCSS = ({ indent, str, childs }, { selector: selectorPrefix = '' } = {}
   if (unnestedAtRule.some(atRule => str.startsWith(atRule))) {
     return `${str};`
   } else if (childs.length) {
+    const selectorRulesets = matchSelectorRulesets(str)
     const selector =
       isAtRule
         ? str
-        : matchSelectorRulesets(str)
-            .map(str =>
-              str.includes('&')
-              // todo @banou26: make `any` and `matches` work on any browsers
-                ? str.replace('&', `:-webkit-any(${selectorPrefix})`)
-                : `${selectorPrefix ? `:-webkit-any(${selectorPrefix})` : selectorPrefix} ${str}`)
-            .join(',')
-            .trim()
+        : selectorRulesets.length > 1
+          ? selectorRulesets
+              .map(str =>
+                str.includes('&')
+                // todo @banou26: make `any` and `matches` work on any browsers
+                  ? str.replace('&', `:-webkit-any(${selectorPrefix})`)
+                  : `${selectorPrefix ? `:-webkit-any(${selectorPrefix})` : selectorPrefix} ${str}`)
+              .join(',')
+              .trim()
+          : selectorRulesets[0].includes('&')
+              ? selectorRulesets[0].replace('&', `${selectorPrefix}`)
+              : `${selectorPrefix ? `${selectorPrefix}` : selectorPrefix} ${selectorRulesets[0]}`
     return `${
       selector
     }{${
