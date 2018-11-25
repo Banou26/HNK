691f22eb5aea2915f9a699b615b0963ff40a6e7f
Banou26 <me@banou.dev>
2018-11-25 05:41:35 +0100

    [Reactivity] Fix array constructor handling(bug with .map, ect... usage)

--- src/reactivity/types/array.js
+++ src/reactivity/types/array.js
@@ -1,19 +1,16 @@
 import { setReactivity } from '../utils.js'
 import proxify from '../proxy.js'
+import { r } from '../index.js'
 
 export const type = Array
 
 let original
 export const ReactiveType = class ReactiveArray extends Array {
   constructor (...values) {
-    super()
+    super(...values.map(val => r(val)))
     const proxy = proxify(this)
     setReactivity({target: proxy, original, object: this})
     if (original) original = undefined
-    if (values) {
-      for (const val of values) proxy.push(val)
-    }
-    values.forEach((val, i) => (proxy[i] = val))
     return proxy
   }
 }
