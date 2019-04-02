e6203c4e68ca98463e0350b4b1dcf3f1f94453f4
Banou26 <thomas26_ds@msn.com>
2019-04-02 18:45:16 +0200

    other(html): Add old html code for reference

---
+++ packages/css/src/_css/elements/index.ts
@@ -0,0 +1,7 @@
+import createStyle from './style'
+import { OzStyle } from './utils'
+
+export {
+  createStyle,
+  OzStyle
+}
---
+++ packages/html/src/_html/elements/index.ts
@@ -0,0 +1,7 @@
+import createTemplate from './template'
+import { OzHTMLTemplate } from './utils'
+
+export {
+  createTemplate,
+  OzHTMLTemplate
+}
