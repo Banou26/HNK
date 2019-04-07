a4c8a541b6f19923f1c1fff151366aea7380b207
Banou26 <me@banou.dev>
2019-04-07 05:22:47 +0200

    refactor(hooks): Improve code quality
    Add TS types
    Improve file paths
    Remove useless parts

--- packages/hooks/tests/useState.spec.js
+++ packages/hooks/tests/useState.spec.js
@@ -1,19 +1,15 @@
 import { test, assert } from 'epk'
-import { withHooks, useState } from '../src/index'
+import { withHooks, useState } from '../src/index.ts'
 import { take, map, toArray } from 'rxjs/operators'
 
 test('is undefined outside of withHooks', () =>
   assert(useState === undefined))
 
-test('throws if outside of withHooks', () =>
-  assert.throws(() =>
-    useState()))
-
 test('make withHooks re-evaluate on setValue', () =>
   withHooks(() => {
     const [ value, setValue ] = useState(false)
 
-    if (!value) setTimeout(() => setValue(true), 10)
+    if (!value) setTimeout(() => setValue(true))
 
     return value
   })
