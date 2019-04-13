9900a58d5516d582e785e5d1c86c067caa990950
Banou26 <me@banou.dev>
2019-04-13 19:05:58 +0200

    fix: Change effects/useState to behave more like React Hooks

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,38 +1,66 @@
 import { Observable } from 'rxjs'
 
+enum HookType {
+  state,
+  effect
+}
+
 export let useState:  <T>(initialValue: T) => [T, (value: T) => void]
 export let useEffect: (effect: Function, values: any[]) => void
 
 export const withHooks = <T>(fn: () => T): Observable<T> =>
   Observable.create(observer => {
-    const state = {}
+    const states = []
+    const effects = []
+
     const run = (firstRun = false) => {
       let index = 0
+      let nextRunQueued
+
       useState = initialValue => {
         const currentIndex = index
-        if (firstRun) state[currentIndex] = initialValue
+        if (firstRun) states.splice(currentIndex, 1, initialValue)
+        const value = states[currentIndex]
         const tuple: [any, (value: any) => void] =
           [
-            state[currentIndex],
+            value,
             (newValue: any) => {
-              if (Object.is(newValue, state[currentIndex])) return
-              state[currentIndex] = newValue
-              observer.next(run())
+              if (Object.is(newValue, value)) return
+              states.splice(currentIndex, 1, newValue)
+              if (!nextRunQueued) {
+                nextRunQueued = true
+                setTimeout(() => observer.next(run()))
+              }
+
             }
           ]
         index++
         return tuple
       }
-      useEffect = (effect, values) => {
-        if (firstRun) {
-          setTimeout(effect)
-        } else if (!values || (state[index] && state[index]?.some((value, i) => !Object.is(value, values[i])))) {
-          setTimeout(effect)
+      useEffect = (effect, newValues) => {
+        if (!(newValues === undefined || Array.isArray(newValues))) throw new Error('useEffect second argument should either be undefined or an Array')
+        const currentIndex = index
+        const values = effects[currentIndex]?.[1]
+        if (firstRun || !newValues || newValues?.some((value, i) =>!Object.is(value, values?.[i]))) {
+          effects.splice(currentIndex, 1, [effect, newValues])
         }
-        state[index] = values
       }
 
+      const lastEffects = Array.from(effects)
       const value = fn()
+
+      setTimeout(() => {
+        lastEffects
+          .filter((_, i) =>
+            effects[i][1]?.some((value, i2) =>
+              !Object.is(value, effects[i][i2])))
+          .forEach(([cleanup]) => cleanup?.())
+
+        effects
+          .forEach(([effect], i) =>
+            typeof effect === 'function' &&
+            (effects[i][0] = effect()))
+      })
 
       useState = undefined
       useEffect = undefined
