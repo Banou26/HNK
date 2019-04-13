ecea048ec099f80b8e16e7554bd1751b89a0c303
Banou26 <thomas26_ds@msn.com>
2019-04-13 19:05:58 +0200

    fix: Change effects/useState to behave more like React Hooks

--- packages/hooks/tests/useEffect.spec.js
+++ packages/hooks/tests/useEffect.spec.js
@@ -1,11 +1,17 @@
 import { test, assert } from 'epk'
 import { withHooks, useState, useEffect } from '../src/index.ts'
-import { take, map, toArray } from 'rxjs/operators'
+import { timer } from 'rxjs'
+import { take, map, toArray, takeUntil } from 'rxjs/operators'
 
 test('is undefined outside of withHooks', () =>
   assert(useEffect === undefined))
 
-test('run on every withHooks run if no second argument passed', () =>
+test('is a function inside withHooks', () =>
+  withHooks(() =>
+    assert(typeof useEffect === 'function'))
+  |> take(1))
+
+test('always run if no second argument passed', () =>
   withHooks(() => {
     const [ value, setValue ] = useState(0)
 
@@ -15,21 +21,26 @@ test('run on every withHooks run if no second argument passed', () =>
 
     return value
   })
-  |> take(3)
+  |> takeUntil(timer(50))
   |> toArray()
   |> map(values => assert.deepStrictEqual(values, [0, 1, 2])))
 
 
-  test('run on first withHooks run if empty array passed as second argument', () =>
-    withHooks(() => {
-      const [ value, setValue ] = useState(0)
+test('only run if second argument is passed and has different values', () =>
+  withHooks(() => {
+    const [ effect1, setEffect1 ] = useState(0)
+    const [ effect2, setEffect2 ] = useState(0)
 
-      useEffect(() => {
-        if (value <= 2) setTimeout(() => setValue(value + 1))
-      }, [])
+    useEffect(() => {
+      setEffect1(effect1 + 1)
+    }, [])
 
-      return value
-    })
-    |> take(2)
-    |> toArray()
-    |> map(values => assert.deepStrictEqual(values, [0, 1])))
+    useEffect(() => {
+      if (effect2 < 2) setEffect2(effect2 + 1)
+    }, [effect2])
+
+    return [effect1, effect2]
+  })
+  |> takeUntil(timer(50))
+  |> toArray()
+  |> map(values => assert.deepStrictEqual(values, [[0, 0], [1, 1], [1, 2]])))
