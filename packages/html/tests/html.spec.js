e59e36705536e6796404d6cc790d7e65b92aad3f
Banou26 <thomas26_ds@msn.com>
2019-04-22 05:16:25 +0200

    test(html): Organize tests (keyed, non keyed, ect...)

--- packages/html/tests/html.spec.js
+++ packages/html/tests/html.spec.js
@@ -4,14 +4,5 @@ import { html } from '../src/index.ts'
 test('is a function', () =>
   assert(typeof html === 'function'))
 
-test('if used as template literal tag, instanceof HTMLTemplateElement', () =>
+test('instanceof HTMLTemplateElement', () =>
   assert(html`` instanceof HTMLTemplateElement))
-
-test('if used as function, typeof function', () =>
-  assert(typeof html() === 'function'))
-
-test('if used as function, returned function instanceof HTMLTemplateElement', () =>
-  assert(html()`` instanceof HTMLTemplateElement))
-
-test('if used as function, sets HNKHTMLTemplateElement#key value to first argument', () =>
-  assert(html('foo')``.key === 'foo'))
